6654af12c47ff54f0aa7c36a9057d0a758140514
danielhzanotto <danielhaas94@gmail.com>
2022-11-21 21:25:57 -0300

    Update ex002.py

---
+++ days 11-20/day016 OOP/ex002.py
@@ -0,0 +1,11 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+
+table.add_column("Pokemon Name", ["Pikachu",
+                 "Squirtle", "Charmander", "Bulbasaur"])
+
+table.add_column("Type", ["Electric", "Water", "Fire", "Grass"])
+
+print(table)
