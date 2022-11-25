c6e4874cc20ff25e53fdfce218524d6070d6df5b
danielhzanotto <danielhaas94@gmail.com>
2022-11-25 13:06:46 -0300

    corrections

--- days 11-20/day018 Graphics/ex007 turtle_painting.py
+++ days 11-20/day018 Graphics/ex007 turtle_painting.py
@@ -1,20 +1,21 @@
 from turtle import Turtle, Screen
 import random
+import colorgram
 
+rgb_colors = []
+colors = colorgram.extract('spot-painting.jpg', 20)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
 tim = Turtle()
 tim.shape("turtle")
 
 screen = Screen()
 screen.colormode(255)
-
-
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    return r, g, b
-
 
 range_a = int(screen.screensize()[0] / 20)
 range_b = int(screen.screensize()[1] / 20)
@@ -29,7 +30,7 @@ for h in range(range_b):
 
     for v in range(range_a):
         tim.down()
-        tim.color(random_color())
+        tim.color(rgb_colors[random.randint(1, len(rgb_colors) - 1)])
         tim.dot(10)
         tim.up()
         tim.forward(range_a)
