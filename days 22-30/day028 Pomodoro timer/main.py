dab9881593864da65de8e9192b9fcc0eb54a5a37
danielhzanotto <danielhaas94@gmail.com>
2022-12-13 09:43:52 -0300

    finish day 28

--- days 22-30/day028 Pomodoro timer/main.py
+++ days 22-30/day028 Pomodoro timer/main.py
@@ -1,6 +1,11 @@
 from tkinter import *
 import time
+import math
+
+
 # ---------------------------- CONSTANTS ------------------------------- #
+
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -9,57 +14,64 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
-is_on = True
-rounds = 0
-exit_timer = False
 
-
-def timer(sleep):
-    print(sleep)
-    time.sleep(sleep)
-
-
-def exit_b():
-    exit_timer = True
-
-
-def reset_b():
-    pass
-
-
-# while is_on:
-#     timer(WORK_MIN)
-
-#     rounds += 1
-
-#     if rounds < 4:
-#         timer(SHORT_BREAK_MIN)
-#     elif rounds == 4:
-#         timer(LONG_BREAK_MIN)
-#         rounds = 0
-
-#     if exit_timer:
-#         is_on = False
+def reset_timer():
+    window.after_cancel(timer)
+    timer_label["text"] = "TIMER"
+    timer_check["text"] = ""
+    canvas.itemconfig(timer_text, text="00:00")
+    global reps
+    reps = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
 def start_timer():
-    countdown(5)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        timer_label["text"] = "BREAK"
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        timer_label["text"] = "BREAK"
+    else:
+        countdown(work_sec)
+        timer_label["text"] = "WORK"
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
 def countdown(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        if reps % 2 != 0:
+            timer_check["text"] += "✔"
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=30, pady=30, bg=YELLOW)
@@ -76,16 +88,14 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
                                 font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-button_exit = Button(text="EXIT", command=exit_b, highlightthickness=0)
+button_exit = Button(text="START", command=start_timer, highlightthickness=0)
 button_exit.grid(row=2, column=0)
 
-button_reset = Button(text="RESET", command=reset_b, highlightthickness=0)
+button_reset = Button(text="RESET", command=reset_timer, highlightthickness=0)
 button_reset.grid(row=2, column=2)
 
-timer_label = Label(text="✔", font=(
+timer_check = Label(font=(
     FONT_NAME, 18, "bold"), bg=YELLOW, fg=PINK)
-timer_label.grid(row=3, column=1)
-
-start_timer()
+timer_check.grid(row=3, column=1)
 
 window.mainloop()
